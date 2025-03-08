5a6c64a5049ea638f3548cff9249bc926d66e1c1
Shulka12 <101873230+Shulka12@users.noreply.github.com>
2025-03-08 11:49:57 +0530

    Add files via upload

---
+++ changes1.py
@@ -0,0 +1,16 @@
+import math
+
+def sum_of_two_numbers(a, b):
+    return a + b
+
+# Example usage
+result = sum_of_two_numbers(3, 5)
+print("The sum is:", result)
+
+
+def area_of_circle(radius):
+    return math.pi * radius ** 2
+
+# Example usage
+circle_area = area_of_circle(5)
+print("The area of the circle is:", circle_area)
